69879f3e2932f3582209690112374230f57b76e9
juliaross2 <159857237+juliaross2@users.noreply.github.com>
2024-06-26 17:00:20 -0700

    added index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
-  let apiKey = "a5acb752426cd8188485c35694980e3a";
+  let apiKey = "4b07aeeoe5t1743addfeb8800f3c54ea";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
