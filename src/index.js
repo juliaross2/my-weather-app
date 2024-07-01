0f9ebbbcedd9e5128274437b5342732ef2376901
juliaross2 <159857237+juliaross2@users.noreply.github.com>
2024-07-01 11:34:07 -0700

    updated weather icon with API

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ function refreshWeather(response) {
   let windspeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
-
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -41,6 +41,7 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
